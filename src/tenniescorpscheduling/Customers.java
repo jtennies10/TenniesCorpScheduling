f67a7a2f0c71c4d7b4930b1616f8bb4d65029d9c
jtennies10 <jdtennies10@gmail.com>
2018-10-05 19:17:03 -0400

    Created Customers method declarations

---
+++ src/tenniescorpscheduling/Customers.java
@@ -0,0 +1,33 @@
+/*
+ * Class to provide the methods used for viewing, creating, updating,
+ * and deleting customers from the database.
+ */
+package tenniescorpscheduling;
+
+/**
+ *
+ * @author Joshua
+ */
+public class Customers {
+    private Customers() {}
+    
+    public static void viewCustomers() {
+        
+    }
+    
+    public static boolean updateCustomer() {
+        return false;
+    }
+    
+    public static boolean createCustomer() {
+        return false;
+    }
+    
+    public static boolean deleteCustomer(int customerid) {
+        return false;
+    }
+    
+    public static boolean findCustomer(int customerid) {
+        return false;
+    }
+}
