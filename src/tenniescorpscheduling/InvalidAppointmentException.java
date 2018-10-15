7a6f2d7361fe1132180a36cf4f99b24496f6b5c7
Joshua Tennies <jdtennies10@gmail.com>
2018-10-15 08:33:03 -0400

    Added custom exception class

---
+++ src/tenniescorpscheduling/InvalidAppointmentException.java
@@ -0,0 +1,17 @@
+/*
+ * Exception class that is thrown when an appointment is attempted to be made
+ * that is either outside business hours in local time or overlaps with another
+ * appointment the user has. Also thrown when an appointment has a start time
+ * that does not meet the set time or an invalid length.
+ */
+package tenniescorpscheduling;
+
+/**
+ *
+ * @author Joshua Tennies
+ */
+public class InvalidAppointmentException extends RuntimeException {
+    public InvalidAppointmentException(String message) {
+        super(message);
+    }
+}
